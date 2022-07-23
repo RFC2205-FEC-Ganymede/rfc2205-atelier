606d7df55cd9a45f90957d03d7431d61564a92d7
Dylan Hollier <dylanphollier@gmail.com>
2022-07-23 14:36:42 -0500

    Update styleSelectionHelper.jsx

--- client/src/productOverview/styleSelectionHelper.jsx
+++ client/src/productOverview/styleSelectionHelper.jsx
@@ -17,21 +17,8 @@ class StyleSelectionHelper extends React.Component {
       if (state.selected === undefined) {
         return {selected: nextProps.productStyles.results[0].style_id};
       }
-      // if (state.product_id === undefined) {
-      //   console.log('somehting')
-      //   return {product_id: nextProps.productStyles.product_id};
-      // }
-      // if (nextProps.product_id !== state.product_id) {
-      // var x = document.getElementsByClassName('styles-section-forThumb')
-      // console.log('line 25')
-      // console.log(x);
-      // for (var i = 0; i < x[0].childNodes.length; i++) {
-      //     x[0].childNodes[i].style.border = '';
-      //   }
-      // }
-      }
     }
-
+  }
 
   onSale = (sale_price) => {
     if (sale_price !== null) {
@@ -39,51 +26,21 @@ class StyleSelectionHelper extends React.Component {
       }
   }
 
-
-  // prevSelectedStyleStyling = () => {
-  //   const element1 = document.querySelector(`.styles-selection-img${prevIndex}`);
-  //   // console.log('HIT', prevIndex)
-  //   if (element1 !== null) {
-  //     element1.style.border = '4px solid #428047';
-  //   }
-  // }
-
-
   clickevenlist = (e) => {
-    // console.log(e.target.className);
     this.setState({
       selected: e.target.className,
     }, () => {
       this.props.onThumbnailClick(e)
       var x = document.getElementsByClassName('styles-section-forThumb')
-      // console.log(x);
       for (var i = 0; i < x[0].childNodes.length; i++) {
-        // console.log(x[0].childNodes[i].className)
-        // console.log(Number(this.state.selected) === Number(x[0].childNodes[i].className))
         if (Number(this.state.selected) === Number(x[0].childNodes[i].className)) {
-          // console.log('Match')
           x[0].childNodes[i].style.border = '5px solid #428047';
         } else {
           x[0].childNodes[i].style.border = '';
         }
       }
     })
-
-
   }
-
-  // currentSelectedStyleStyling = () => {
-  //   const element = document.querySelector(`.styles-selection-img${this.props.currentIndex}`);
-  //   if (411534 === this.props.productStyles.results[Number(this.props.currentIndex)].style_id && element !== null) {
-  //     element.style.border = '5px solid red';
-  //   }
-  //   // for (var i = 0; i < )
-  //   if (Number(this.props.thumbStyle) === this.props.productStyles.results[Number(this.props.currentIndex)].style_id ) {
-  //     element.style.border = '5px solid red';
-  //   } else if (Number(this.props.thumbStyle) !== this.props.productStyles.results[Number(this.props.currentIndex)].style_id && element !== null) {
-  //     element.style.border = '4px solid #428047';
-  //   }
-  // }
 
   render () {
     if (this.props.productStyles.results !== undefined && this.props.productStyles.results.length !== 0 && this.props.productStyles.results[0].photos[0].thumbnail_url !== null) {
